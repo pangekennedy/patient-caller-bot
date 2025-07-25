b2d74db19f4a266585558814d7bd2b5feb2b5252
pangekennedy <57912791+pangekennedy@users.noreply.github.com>
2025-07-25 07:37:01 +0000

    Initial commit for Flask voice bot

--- app.py
+++ app.py
@@ -59,5 +59,13 @@ def gather():
     return Response(str(response), mimetype='application/xml')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from env or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
